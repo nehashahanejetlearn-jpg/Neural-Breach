cc95d1395e5e51e68608e4a0c42cbb30167169fb
nehashahanejetlearn-jpg <nehashahane.jetlearn@gmail.com>
2026-09-20 20:44:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig(() => {
         '@': path.resolve(__dirname, '.'),
       },
     },
-    // ...
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  };
+});
